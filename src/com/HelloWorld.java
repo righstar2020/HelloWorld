1876cf04117d56865821c9b04281cdde891f3ebb
rightstar2020 <1145133484@qq.com>
2020-09-04 22:59:36 +0800

    更新

--- src/com/HelloWorld.java
+++ src/com/HelloWorld.java
@@ -3,8 +3,9 @@ package com;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-
-			System.out.println("Hello World");
+			System.out.println("---Rightstar2020---");
+			System.out.println("    Hello World");
+			
 
 	}
 
